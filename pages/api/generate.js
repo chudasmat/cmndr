2b32d8d0cb516a1bde6e31bf9d225bff39220cff
Trishan Chudasma <trishan759@gmail.com>
2023-01-09 17:56:17 +0000

    .

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,32 +1,50 @@
-import {Configuration, OpenAIApi} from 'openai';
+import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+	apiKey: process.env.OPENAI_API_KEY,
 });
-
 
 const openai = new OpenAIApi(configuration);
 
-const basePromptPrefix = 
-`
-Generate an answer for the following in a succint manner.
-
-`
-
+const basePromptPrefix = `
+Generate a GCSE Economics six mark question that is related to the topic given. If a question is given then only output the input.
+Topic:
+`;
 
 const generateAction = async (req, res) => {
-  // Run first prompt
-  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
-  const baseCompletion = await openai.createCompletion({
-    model: 'text-davinci-003',
-    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
-    temperature: 0.75,
-    max_tokens: 700
-  });
+	console.log(`API: ${basePromptPrefix}${req.body.userInput}`);
 
-  const basePromptOutput = baseCompletion.data.choices.pop();
+	const baseCompletion = await openai.createCompletion({
+		model: "text-davinci-003",
+		prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+		temperature: 0.8,
+		max_tokens: 300,
+	});
 
-  res.status(200).json({ output: basePromptOutput });
+	const basePromptOutput = baseCompletion.data.choices.pop();
+
+	// I build Prompt #2.
+	const secondPrompt = `
+ Answer the following GCSE Economics question, using the keywords "because", "leads to" and "therefore". The answer must be written in a succinct manner with high attention to detail, and should relate to the OCR specification.
+ Question: ${basePromptOutput.text}
+ Answer: 
+ `;
+
+	// I call the OpenAI API a second time with Prompt #2
+	const secondPromptCompletion = await openai.createCompletion({
+		model: "text-davinci-003",
+		prompt: `${secondPrompt}`,
+		// I set a higher temperature for this one. Up to you!
+		temperature: 0.85,
+		// I also increase max_tokens.
+		max_tokens: 1250,
+	});
+
+	// Get the output
+	const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+
+	// Send over the Prompt #2's output to our UI instead of Prompt #1's.
+	res.status(200).json({ output: secondPromptOutput });
 };
 
 export default generateAction;
